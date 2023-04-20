9da03a5494335ec039a4cb48242a6faa70ba13e4
Karol Zyśk <zysk.karol.pawel@gmail.com>
2023-04-20 22:19:25 +0200

    add gitignore

---
+++ CProject/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CProject
+{
+    internal class Person
+    {
+        public string FirstName;
+        public string LastName;
+
+        private DateTime dateOfBirth;
+
+        public Person(string firstName, string lastName) {
+            FirstName = firstName;
+            LastName = lastName;
+
+        }
+        public Person(string firstName, string lastName, DateTime dateOfBirth) : this(firstName, lastName) 
+        {
+            
+            SetDateOfBirth(dateOfBirth);
+
+        }
+
+        public void SetDateOfBirth(DateTime date)
+        {
+            if (date > DateTime.Now)
+            {
+                Console.WriteLine("Invalid dob");
+            }
+            else
+            {
+                dateOfBirth = date;
+            }
+        }
+        public DateTime GetDateOfBirth() => dateOfBirth;
+
+        public void SayHi  ()
+        {
+            Console.WriteLine($"My name is {FirstName}, my latname is {LastName} and dob is {dateOfBirth}");
+        }
+        
+    }
+}
--- CProject/Program.cs
+++ CProject/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CProject
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+            Person Somebody = new Person("Karol", "Zyśk");
+            Person Somebody2 = new Person("Paweł", "Zyśk", new DateTime(2000,05,18));
+
+          
+            Somebody.SetDateOfBirth(new DateTime(1990, 05, 18));
+            DateTime dob = Somebody.GetDateOfBirth();
+            Somebody.SayHi();
+            Somebody2.SayHi();
+
+
+        }
+
+    }
+}
